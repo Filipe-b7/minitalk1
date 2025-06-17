1be2df0d4fe2475bae98c57853c145c1102e383f
frocha-b <frocha-b@student.42porto.comm>
2025-06-17 11:10:33 +0100

    a

--- server.c
+++ server.c
@@ -6,19 +6,45 @@
 /*   By: frocha-b <frocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/05 19:16:03 by frocha-b          #+#    #+#             */
-/*   Updated: 2025/06/09 16:16:15 by frocha-b         ###   ########.fr       */
+/*   Updated: 2025/06/10 17:13:29 by frocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void handler(int signal)
+
+// void	bits_to_string(int *bits_array)
+// {
+// 	static int i = 0;
+// 	static char *string;
+	
+// 	while (!bits_array_is_null(bits_array))
+// 	{
+		
+// 	}
+	
+	
+// }
+void	handler(int signal)
 {
-	// ft_printf("signal: %d\n", signal);
-	if (signal == SIGUSR1)
-		ft_printf("bit 0 recebido\n");
-	else 
-		ft_printf("bit 1 recebido\n");
+	static int 	i = 0;
+	static int bits_array [8];
+	
+	while (i < 8)
+	{
+		if (signal == SIGUSR1)
+		{
+			bits_array[i++] = 0;
+			ft_printf("bit 0 recebido\n");
+		}
+		else 
+		{
+			bits_array[i++] = 1;
+			ft_printf("bit 1 recebido\n");
+		}
+	}
+	// bits_to_string(bits_array);
+	i = 0;
 }
 int main(void)
 {
